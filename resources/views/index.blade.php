9856d59046f44365462827dc3190791585163da1
vfj-honda <mhonda@vitalify.jp>
2021-04-08 14:57:14 +0900

    add views/index.blade.php and edit routes/web.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+
+        <div class="container">
+            Hello Laravel!!!
+        </div>
+    </body>
+</html>
+
